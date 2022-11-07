db0584ba13ce38c07ee25229262e0b25eb0c092a
moriyamataku <moriyamataku2@gmail.com>
2022-11-07 09:09:41 +0900

    Time: 96 ms (56.29%), Space: 42.4 MB (33.57%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
@@ -6,36 +6,31 @@ var tictactoe = function(moves) {
     const a = []
     const b = []
     for(let i = 0; i < moves.length; i++) {
-        if(i % 2 === 0) {
-            a.push(moves[i])
-        } else {
-            b.push(moves[i])
-        }
+        const val = movetoval(moves[i]);
+        (i % 2 === 0) ? a.push(val) : b.push(val);
     }
     if(check(a)) return "A"
     if(check(b)) return "B"
     return moves.length === 9 ? "Draw" : "Pending"
 };
 
-var check = function(moves) {
-    // console.log(moves)
+var movetoval = function(move) {
+    return move[0] + move[1] * 3
+}
+
+var check = function(vals) {
+    // console.log(vals)
     for(let i = 0; i < 3; i++) {
-        if(hasArray(moves, [i, 0]) && hasArray(moves, [i, 1]) && hasArray(moves, [i, 2])) return true
+        if(vals.includes(i + 0) && vals.includes(i + 3)&& vals.includes(i + 6)) return true
     }
     
     for(let i = 0; i < 3; i++) {
-        if(hasArray(moves, [0, i]) && hasArray(moves, [1, i]) && hasArray(moves, [2, i])) return true
+        if(vals.includes(i * 3 + 0) && vals.includes(i * 3 + 1)&& vals.includes(i * 3 + 2)) return true
     }
     
-    if(hasArray(moves, [0, 0]) && hasArray(moves, [1, 1]) && hasArray(moves, [2, 2])) return true
-    if(hasArray(moves, [2, 0]) && hasArray(moves, [1, 1]) && hasArray(moves, [0, 2])) return true
+    if(vals.includes(0) && vals.includes(4)&& vals.includes(8)) return true
+    if(vals.includes(2) && vals.includes(4)&& vals.includes(6)) return true
     
     return false
 };
 
-var hasArray = function(moves, arr) {
-    for(let i = 0; i < moves.length; i++) {
-        if(moves[i].join('-') === arr.join('-')) return true
-    }
-    return false
-}
