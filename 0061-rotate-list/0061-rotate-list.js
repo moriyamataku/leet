e2a6ae11b106e7e022ed7cc3b2b41e85a910848a
moriyamataku <moriyamataku2@gmail.com>
2022-11-10 10:08:02 +0900

    Time: 7989 ms (5.00%), Space: 42.5 MB (98.73%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if(!head || k < 1) return head
+    let count = 1
+    const tail = reverse(head)
+    // console.log(head, tail)
+    let current = tail
+    let previous = null
+    while(current) {
+        if(count === k) break;
+        previous = current
+        current = current.next || tail
+        count++;
+    } 
+    console.log(current, previous)
+    const next = current.next
+    current.next = null
+    if(head !== tail && next !== null) head.next = tail
+    return (next !== null) ? reverse(next) : reverse(tail)
+};
+
+var reverse = function(head) {
+    let tail = head
+    let current = head
+    let previous = null
+    while(current) {
+        const next = current.next
+        current.next = previous
+        previous = current
+        current = next
+    }
+    return previous
+}
