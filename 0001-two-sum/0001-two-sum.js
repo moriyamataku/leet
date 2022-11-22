2dcb97104b30a4dd34df23d539efdc08ba427060
moriyamataku <moriyamataku2@gmail.com>
2022-11-22 10:47:38 +0900

    Time: 111 ms (68.97%), Space: 44.7 MB (10.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,9 +6,10 @@
 var twoSum = function(nums, target) {
     const table = {}
     for(let i = 0; i < nums.length; i++){
-        const ans = target - nums[i]
-        // console.log(i, nums[i], ans ,table)
-        if(table.hasOwnProperty(ans.toString())) return [i, table[ans]]
-        table[nums[i]] = i
+        const current = nums[i]
+        const answer = (target - current).toString()
+        // console.log(answer, table)
+        if(table[answer] !== undefined) return [table[answer], i]
+        table[current] = i
     }
 };
