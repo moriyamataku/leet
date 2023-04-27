a95d5101250892a501e630f0926658aa972e5607
moriyamataku <moriyamataku2@gmail.com>
2023-04-27 17:33:59 +0900

    Time: 94 ms (60.19%), Space: 53.3 MB (67.80%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const counts = nums.reduce((pre, cur) => {
-        pre[cur] ? pre[cur] += 1 : pre[cur] = 1 
-        return pre
-    },{})
-    
-    return Object.values(counts).some(count => count > 1)
+    const s = new Set()
+    for(let i = 0; i < nums.length; i++) {
+        if(s.has(nums[i])) return true
+        s.add(nums[i])
+    }
+    return false
 };
