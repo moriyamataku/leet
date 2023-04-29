3a4070114147aa5e4c7896adfbbae5b0ed49822c
moriyamataku <moriyamataku2@gmail.com>
2023-04-29 12:08:39 +0900

    Time: 68 ms (81.65%), Space: 43.9 MB (99.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,27 +11,27 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    // let slow = head
-    // if(!head || !head.next) return false
-    // let fast = head.next
-    // while(fast && fast.next) {
-    //     if(slow.val === fast.val) return true
-    //     slow = slow.next
-    //     fast = fast.next.next
-    // }
-    // return false
-    
-    let [ slow, fast ] = [ head, head];
-
-    while (fast && fast.next) {/* Time O(N) */
-        slow = slow.next;
-        fast = fast.next.next;
-
-        const hasCycle = slow === fast;
-        if (hasCycle) return true;
+    let slow = head
+    if(!head || !head.next) return false
+    let fast = head.next
+    while(fast && fast.next) {
+        if(slow === fast) return true
+        slow = slow.next
+        fast = fast.next.next
     }
+    return false
+    
+//     let [ slow, fast ] = [ head, head];
 
-    return false;
+//     while (fast && fast.next) {/* Time O(N) */
+//         slow = slow.next;
+//         fast = fast.next.next;
+
+//         const hasCycle = slow === fast;
+//         if (hasCycle) return true;
+//     }
+
+//     return false;
 };
 
 
