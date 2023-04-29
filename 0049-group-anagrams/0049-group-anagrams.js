b0c5c6afa7f0e85f4e1b5dfd491128107f10f4e1
moriyamataku <moriyamataku2@gmail.com>
2023-04-29 09:47:58 +0900

    Time: 222 ms (9.91%), Space: 68.8 MB (5.30%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const result = {}
+    strs.forEach(str => {
+        const count = str.split('').reduce((pre, cur) => {
+            pre[cur] ? pre[cur] += 1 : pre[cur] = 1
+            return pre
+        },{})
+        const key = Object.entries(count).sort((a, b) => a[0].charCodeAt(0) - b[0].charCodeAt(0)).map((arr) => `${arr[0]}-${arr[1]}`).join(',')
+        
+        result[key] ? result[key].push(str) : result[key] = [str]
+    })
+    // console.log(result)
+    return Object.values(result)
+};
